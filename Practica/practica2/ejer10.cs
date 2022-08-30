6479e02cbc28e5add0ad80c350444911c342a72a
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 18:27:42 -0300

    Update ejer10.cs

--- Practica/practica2/ejer10.cs
+++ Practica/practica2/ejer10.cs
@@ -5,15 +5,30 @@ implementados en el ejercicio anterior.
 El módulo datetime proporciona clases para manipular fechas y horas. Si bien la implementación permite operaciones aritméticas con fechas y horas, 
 su principal objetivo es poder extraer campos de forma eficiente para su posterior manipulación o formateo.
 
+/*
 
+using System.Text;
 // Inicia el contador:
 DateTime tiempo1 = DateTime.Now;
  
-// Código del programa...
+// Código del programa a medir tiempo de ejecución (ejercicio 9)
+{
+ String myString = "Hola";
+ for (int i = 0; i < 1000; i++)
+     myString += "I"; // poner 100 I en el string
+ Console.WriteLine("String: " + myString);
  
+ StringBuilder myStringBuilder = new StringBuilder();
+ for (int i = 0; i < 1000; i++)
+     myStringBuilder.Append('I'); 
+ Console.WriteLine("String Builder: " + myStringBuilder);
+}
+
 // Para el contador e imprime el resultado:
 DateTime tiempo2 = DateTime.Now;
 TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
-Console.Write( "TIEMPO: " + total.ToString() );
+Console.Write( "TIEMPO: " + total.ToString() ); 
+//Para el String tardo : 00:00:00.1020014
+//Para el StringBuilder: 00:00:00.1018641
+//Los 2 juntos: 00:00:00.1039235
 
-*/
