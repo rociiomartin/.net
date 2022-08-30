197e9beb2c18684eed43b18174f6d02de7c30436
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 18:38:30 -0300

    Update ejer11.cs

--- Practica/practica2/ejer11.cs
+++ Practica/practica2/ejer11.cs
@@ -1,4 +1,14 @@
 /*
 11. ¿Para qué sirve el método Split de la clase string? Usarlo para escribir en la consola todas
 las palabras (una por línea) de una frase ingresada por consola por el usuario.
+
+El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
+
 */
+
+String st = Console.ReadLine();
+string[] mensaje = st.Split(' ');
+foreach (var word in mensaje)
+{
+    Console.WriteLine($"<{word}>");
+}
