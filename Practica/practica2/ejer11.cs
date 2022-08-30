8fc62eab10cf73520cb823a6ac3de6eed50ff7fa
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-30 18:39:27 -0300

    Update ejer11.cs

--- Practica/practica2/ejer11.cs
+++ Practica/practica2/ejer11.cs
@@ -5,10 +5,9 @@ las palabras (una por línea) de una frase ingresada por consola por el usuario.
 El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
 
 */
-
 String st = Console.ReadLine();
 string[] mensaje = st.Split(' ');
 foreach (var word in mensaje)
 {
-    Console.WriteLine($"<{word}>");
+    Console.WriteLine($"{word}");
 }
