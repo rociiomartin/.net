6ad7385426cfd9b2db8576f2562826208447641e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-08-31 18:36:19 -0300

    Update ejer14.cs

--- Practica/practica2/ejer14.cs
+++ Practica/practica2/ejer14.cs
@@ -5,3 +5,24 @@ EsPrimo(int n) que devuelve true sólo si n es primo. Esta función debe comprob
 divisible por algún número entero entre 2 y la raíz cuadrada de n. (Nota: Math.Sqrt(d)
 devuelve la raíz cuadrada de d)
 */
+int numDado=Convert.ToInt32(args[0]);//como es un string tengo q pasarlo a int
+Console.WriteLine("Numeros primos del 1 al " + numDado);
+for (int i = 1; i <= numDado; i++) 
+//for (int i = 1; i < 11; i++)
+{
+   if (EsPrimo(i))
+   {
+       Console.WriteLine(i);
+   }
+}
+
+static bool EsPrimo(int n)
+{
+    bool EsP=false;
+    if ( ( n % 2 != 0) && ( Math.Sqrt(n) % 2 != 0 ) )
+    {
+        EsP=true;
+    }
+    return EsP;
+}
+
