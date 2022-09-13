698a9511bd8517304a9cdf7acdc01b1392b9b8bd
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-13 18:15:04 -0300

    Update ejer16.cs

--- Practica/practica2/ejer16.cs
+++ Practica/practica2/ejer16.cs
@@ -4,3 +4,18 @@ a) Definiendo una función no recursiva
 b) Definiendo una función recursiva
 c) idem a b) pero con expression-bodied methods (Tip: utilizar el operador condicional
 ternario)*/
+
+/*a
+static int Fac(int n)
+{
+    int factorial=1;
+    for (int i = 1; i < n; i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
+}
+
+int nro = int.Parse(args[0]); //numero pasado por argumento
+Console.WriteLine("El factorial de " + nro + " es: "+ Fac(nro));
+*/
