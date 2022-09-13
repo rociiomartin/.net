9975e7dcdc6927b89cd755ce1e5965f60a762f7b
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-13 17:32:47 -0300

    Create Persona.cs

---
+++ Teoria/teoria5b/Persona.cs
@@ -0,0 +1,12 @@
+class Persona
+{
+public int Edad { get; }
+public string Nombre { get; }
+public Persona(string nombre, int edad)
+{
+Nombre = nombre;
+Edad = edad;
+}
+public void Imprimir() =>
+Console.WriteLine($"{Nombre} ({Edad})");
+}
