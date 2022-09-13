1aa59c112bc6496da1d7944640a0982b78ba020e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-13 18:06:15 -0300

    Update ejer15.cs

--- Practica/practica2/ejer15.cs
+++ Practica/practica2/ejer15.cs
@@ -4,3 +4,12 @@ Fibonacci.
 Fib(n) = 1, si n <=2
 Fib(n) = Fib(n-1) + Fib(n-2), si n > 2
 */
+static int Fib(int n)
+{
+    if(n<=2) return 1;
+    else return Fib(n-1) + Fib(n-2);
+}
+
+int i=1;
+Console.WriteLine(Fib(i));
+Console.WriteLine(Fib(i+2));
