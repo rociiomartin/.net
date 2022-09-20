54b1ef7e1fecd6ab2d0cfb0618b15e276e3d1132
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:04:23 -0300

    Create ejer17.cs

---
+++ Practica/practica3/ejer17.cs
@@ -0,0 +1,6 @@
+/*
+17. Implementar un programa calculadora que calcule una expresión ingresada por el usuario
+correspondiente a una operación binaria de las cuatro elementales (ejemplo “44.5/456”,
+“456*45”, “549-12”, “54+6” ). Utilizar try/catch para validar los números y controlar
+cualquier tipo de excepción que pudiese ocurrir en la evaluación de la expresión.
+*/
