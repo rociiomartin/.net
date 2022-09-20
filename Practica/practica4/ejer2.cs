4b765b1d532aecd12b1b7a7feb460e9e11dccb40
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:06:17 -0300

    Create ejer2.cs

---
+++ Practica/practica4/ejer2.cs
@@ -0,0 +1,5 @@
+/*
+2) Modificar el programa anterior haciendo privados todos los campos. Definir un constructor adecuado y un
+método público Imprimir() que escriba en la consola los campos del objeto con el formato requerido para el
+listado.
+*/
