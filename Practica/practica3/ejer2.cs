0b8e39c9dd9db93f5af8c77f1e78417a73a4d9e9
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 16:56:12 -0300

    Create ejer2.cs

---
+++ Practica/practica3/ejer2.cs
@@ -0,0 +1,10 @@
+/*
+2.Implementar un método para imprimir por consola todos los elementos de una matriz (arreglo
+de dos dimensiones) pasada como parámetro. Debe imprimir todos los elementos de una fila en
+la misma línea en la consola.
+
+void ImprimirMatriz(double[,] matriz)
+
+Ayuda: Si A es un arreglo, A.GetLength(i) devuelve la longitud del arreglo en la
+dimensión i.
+*/
