b625b040a61d08ef2069e852616112dd5d43b78f
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:04:04 -0300

    Create ejer16.cs

---
+++ Practica/practica3/ejer16.cs
@@ -0,0 +1,8 @@
+/*
+16. Implementar un programa que permita al usuario ingresar números por la consola. Debe
+ingresarse un número por línea finalizado el proceso cuando el usuario ingresa una línea vacía.
+A medida que se van ingresando los valores el sistema debe mostrar por la consola la suma
+acumulada de los mismos. Utilizar la instrucción try/catch para validar que la entrada
+ingresada sea un número válido, en caso contrario advertir con un mensaje al usuario y
+proseguir con el ingreso de datos.
+*/
