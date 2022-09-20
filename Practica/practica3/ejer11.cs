d4f1e7aaba38fbb225a26e179d06dc93c07d47ef
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 18:51:58 -0300

    Update ejer11.cs

--- Practica/practica3/ejer11.cs
+++ Practica/practica3/ejer11.cs
@@ -4,3 +4,6 @@ List<int> a = new List<int>() { 1, 2, 3, 4 };
 a.Remove(5);
 a.RemoveAt(4);
 */
+
+//a.Remove(5); no hay error de ejecucion, lo busca como no existe no elimina nada
+//a.RemoveAt(4); //error de ejecucion porque no existe la posicion 4 
