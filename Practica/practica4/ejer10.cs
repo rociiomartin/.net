7b33cc06a776688bd81dd1fd351aa10c81be28e7
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:22:09 -0300

    Create ejer10.cs

---
+++ Practica/practica4/ejer10.cs
@@ -0,0 +1,19 @@
+/*
+10) ¿Qué se puede decir en relación a la sobrecarga de métodos en este ejemplo?
+
+class A
+{
+public void Metodo(short n)
+{
+Console.Write("short {0} - ", n);
+}
+public void Metodo(int n)
+{
+Console.Write("int {0} - ", n);
+}
+public int Metodo(int n)
+{
+return n + 10;
+}
+}
+*/
