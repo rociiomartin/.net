07d20cc9762aed5a8249ba77b61da44558a9c4da
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:00:29 -0300

    Create ejer9.cs

---
+++ Practica/practica3/ejer9.cs
@@ -0,0 +1,7 @@
+/*
+9. Señalar errores de compilación y/o ejecución en el siguiente código
+object obj = new int[10];
+dynamic dyn = 13;
+Console.WriteLine(obj.Length);
+Console.WriteLine(dyn.Length);
+*/
