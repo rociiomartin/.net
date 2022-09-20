2ab048461cf74428c5eaf5738e6a1808c44c1267
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:09:07 -0300

    Create ejer4.cs

---
+++ Practica/practica4/ejer4.cs
@@ -0,0 +1,8 @@
+/*
+4) Codificar la clase Hora de tal forma que el siguiente código produzca la salida por consola que se observa.
+
+Hora h = new Hora(23,30,15);
+h.Imprimir();
+
+monitor: 23 horas, 30 minutos y 15 segundos.
+*/
