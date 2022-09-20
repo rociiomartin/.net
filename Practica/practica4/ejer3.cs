395fef863219180257ae257fe18b44ceb27b3d9d
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:08:00 -0300

    Create ejer3.cs

---
+++ Practica/practica4/ejer3.cs
@@ -0,0 +1,5 @@
+/*
+3) Agregar a la clase Persona un método EsMayorQue(Persona p) que devuelva verdadero si la persona que
+recibe el mensaje tiene más edad que la persona enviada como parámetro. Utilizarlo para realizar un programa
+que devuelva la persona más jóven de la lista.
+*/
