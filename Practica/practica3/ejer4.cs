f666209f37aaab43f79b3cfe1cd9cc6f53719e97
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 16:57:00 -0300

    Create ejer4.cs

---
+++ Practica/practica3/ejer4.cs
@@ -0,0 +1,8 @@
+/*
+4. Implementar los métodos GetDiagonalPrincipal y GetDiagonalSecundaria que
+devuelven un vector con la diagonal correspondiente de la matriz pasada como parámetro. Si la
+matriz no es cuadrada generar una excepción ArgumentException.
+
+double[] GetDiagonalPrincipal(double[,] matriz)
+double[] GetDiagonalSecundaria(double[,] matriz)
+*/
