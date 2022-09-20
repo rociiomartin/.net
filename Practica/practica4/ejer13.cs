bb6ab0c1afee69dfa24bdace93172673385e027c
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:24:41 -0300

    Create ejer13.cs

---
+++ Practica/practica4/ejer13.cs
@@ -0,0 +1,25 @@
+/*
+13) Reemplazar estas líneas de código por otras equivalentes que utilicen el operador null-coalescing (?? ) y / o
+la asignación null-coalescing (??=)
+...
+if (st1 == null)
+{
+if (st2 == null)
+{
+st = st3;
+}
+else
+{
+st = st2;
+}
+}
+else
+{
+st = st1;
+}
+if (st4 == null)
+{
+st4 = "valor por defecto";
+}
+...
+*/
