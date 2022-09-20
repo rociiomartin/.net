520c4655d66a2f9b2da8d309b90067b727a84e3e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 16:57:57 -0300

    Create ejer5.cs

---
+++ Practica/practica3/ejer5.cs
@@ -0,0 +1,5 @@
+/*
+5. Implementar un método que devuelva un arreglo de arreglos con los mismos elementos que la
+matriz pasada como parámetro:
+double[][] GetArregloDeArreglo(double [,] matriz)
+*/
