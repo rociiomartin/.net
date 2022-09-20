27af83e9afc57e6513bd8ad08ba0c8f47e5cf5fc
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:30:07 -0300

    Create ejer7.cs

---
+++ Practica/practica5/ejer7.cs
@@ -0,0 +1,9 @@
+/*
+7) Definir la clase Persona con las siguientes propiedades de lectura y escritura: Nombre de tipo
+string, Sexo de tipo char, DNI de tipo int, y FechaNacimiento de tipo DateTime. Además
+definir una propiedad de sólo lectura (calculada) Edad de tipo int. Definir un indizador de
+lectura/escritura que permita acceder a las propiedades a través de un índice entero. Así, si p es un
+objeto Persona, con p[0] se accede al nombre, p[1] al sexo p[2] al DNI, p[3] a la fecha de
+nacimiento y p[4] a la edad. En caso de asignar p[4] simplemente el valor es descartado. Observar
+que el tipo del indizador debe ser capaz almacenar valores de tipo int, char, DateTime y string.
+*/
