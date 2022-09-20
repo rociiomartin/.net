8f918ee7e56f489973b72e11e68032e1b08e9b3e
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:16:50 -0300

    Create ejer9.cs

---
+++ Practica/practica4/ejer9.cs
@@ -0,0 +1,19 @@
+/*
+8) Implementar la clase Matriz que se utilizará para trabajar con matrices matemáticas. Implementar los dos
+constructores y todos los métodos que se detallan a continuación:
+
+public Matriz(int filas, int columnas)
+public Matriz(double[,] matriz)
+public void SetElemento(int fila, int columna, double elemento)
+public double GetElemento(int fila, int columna)
+public void imprimir()
+public void imprimir(string formatString)
+public double[] GetFila(int fila)
+public double[] GetColumna(int columna)
+public double[] GetDiagonalPrincipal()
+public double[] GetDiagonalSecundaria()
+public double[][] getArregloDeArreglo()
+public void sumarle(Matriz m)
+public void restarle(Matriz m)
+public void multiplicarPor(Matriz m)
+*/
