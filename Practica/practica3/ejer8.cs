624c3110a374b6f4f6ef8fc0adb4cb40865c32af
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 16:59:58 -0300

    Create ejer8.cs

---
+++ Practica/practica3/ejer8.cs
@@ -0,0 +1,20 @@
+/*
+8. ¿Qué líneas del siguiente método provocan error de compilación y por qué?
+var a = 3L;
+dynamic b = 32;
+object obj = 3;
+a = a * 2.0;
+b = b * 2.0;
+b = "hola";
+obj = b;
+b = b + 11;
+obj = obj + 11;
+var c = new { Nombre = "Juan" };
+var d = new { Nombre = "María" };
+var e = new { Nombre = "Maria", Edad = 20 };
+var f = new { Edad = 20, Nombre = "Maria" };
+f.Edad = 22;
+d = c;
+e = d;
+f = e;
+*/
