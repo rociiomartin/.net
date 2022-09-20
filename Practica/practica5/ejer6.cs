dabd2bd944d5bab2be600c22530b1c14035095bc
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:29:48 -0300

    Create ejer6.cs

---
+++ Practica/practica5/ejer6.cs
@@ -0,0 +1,7 @@
+/*
+6) Modificar la definición de la clase Matriz realizada en la práctica 4. Eliminar los métodos
+SetElemento(...) y GetElemento(...). Definir un indizador adecuado para leer y escribir
+elementos de la matriz. Eliminar los métodos GetDiagonalPrincipal() y
+GetDiagonalSecundaria() reemplazándolos por las propiedades de sólo lectura
+DiagonalPrincipal y DiagonalSecundaria.
+*/
