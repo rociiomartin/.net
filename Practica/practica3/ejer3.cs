e99cbb81ca53827ef34e4348621207a0e13dddd5
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 18:23:28 -0300

    Update ejer3.cs

--- Practica/practica3/ejer3.cs
+++ Practica/practica3/ejer3.cs
@@ -7,3 +7,28 @@ decimal.
 
 void ImprimirMatrizConFormato(double[,] matriz, string formatString)
 */
+void ImprimirMatrizConFormato(double[,] matriz, string formatString)
+{
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {   
+          Console.Write(matriz[i,j]+ " ");
+        }
+        Console.WriteLine(); //todos los elementos de una fila en la misma línea en la consola.
+    }
+}
+
+double [,] matriz= new double[2,4]; //2 filas con 4 columnas
+for (int i = 0; i < matriz.GetLength(0); i++) 
+{     
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    { 
+         matriz[i,j]=1.3; //siempre es el mismo numero
+    }
+}
+Console.WriteLine("Imprimir Matriz");
+string st= "uno";
+ImprimirMatrizConFormato(matriz,st);
+
+VEEEEEEEER
