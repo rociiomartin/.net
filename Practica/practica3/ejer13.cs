254f48c1068623f97f26a06a78ae80973482464b
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:02:25 -0300

    Create ejer13.cs

---
+++ Practica/practica3/ejer13.cs
@@ -0,0 +1,9 @@
+/*
+13. Realizar un análisis sintáctico para determinar si los paréntesis en una expresión aritmética están
+bien balanceados. Verificar que por cada paréntesis de apertura exista uno de cierre en la cadena
+de entrada. Utilizar una pila para resolverlo. Los paréntesis de apertura de la entrada se
+almacenan en una pila hasta encontrar uno de cierre, realizándose entonces la extracción del
+último paréntesis de apertura almacenado. Si durante el proceso se lee un paréntesis de cierre y
+la pila está vacía, entonces la cadena es incorrecta. Al finalizar el análisis, la pila debe quedar
+vacía para que la cadena leída sea aceptada, de lo contrario la misma no es válida.
+*/
