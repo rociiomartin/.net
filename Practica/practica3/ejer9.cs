69cf74cdbd9037922089e03bd84bbce2e5aea98c
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 18:47:14 -0300

    Update ejer9.cs

--- Practica/practica3/ejer9.cs
+++ Practica/practica3/ejer9.cs
@@ -2,6 +2,11 @@
 9. Señalar errores de compilación y/o ejecución en el siguiente código
 object obj = new int[10];
 dynamic dyn = 13;
-Console.WriteLine(obj.Length);
-Console.WriteLine(dyn.Length);
+Console.WriteLine(obj.Length);//error de compilacion(*)
+Console.WriteLine(dyn.Length);// error de ejecucion(-)
+*/
+
+/*
+(*)Porque 
+(-)Porque
 */
