f4bdcdc35a1e254ef54d2125949a89f78122b8cf
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:34:52 -0300

    Create ejer2.cs

---
+++ Practica/practica6/ejer2.cs
@@ -0,0 +1,11 @@
+/*
+2) Aunque consultar en el código por el tipo de un objeto indica habitualmente un diseño ineficiente,
+por motivos didácticos vamos a utilizarlo. Completar el siguiente código, que utiliza las clases
+definidas en el ejercicio anterior, para que se produzca la salida indicada:
+
+A[] vector = new A[] { new C(1), new D(2), new B(3), new D(4), new B(5) };
+foreach (A a in vector)
+{
+...
+}
+*/
