5a9739f3ba2e9ad025b4ab0d9de76130adc54138
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:03:43 -0300

    Create ejer15.cs

---
+++ Practica/practica3/ejer15.cs
@@ -0,0 +1,18 @@
+/*
+15. ¿Qué salida por la consola produce el siguiente código?
+
+int x = 0;
+try
+{
+Console.WriteLine(1.0 / x);
+Console.WriteLine(1 / x);
+Console.WriteLine("todo OK");
+}
+catch (Exception e)
+{
+Console.WriteLine(e.Message);
+}
+
+¿Qué se puede inferir respecto de la excepción división por cero en relación al tipo de los
+operandos?
+*/
