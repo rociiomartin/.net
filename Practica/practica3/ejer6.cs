b84cfaa7777c195d24bb7446d4d5f16b060a4253
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 16:58:23 -0300

    Create ejer6.cs

---
+++ Practica/practica3/ejer6.cs
@@ -0,0 +1,11 @@
+/*
+6. Implementar los siguientes métodos que devuelvan la suma, resta y multiplicación de matrices
+pasadas como parámetros. Para el caso de la suma y la resta, las matrices deben ser del mismo
+tamaño, en caso de no serlo devolver null. Para el caso de la multiplicación la cantidad de
+columnas de A debe ser igual a la cantidad de filas de B, en caso contrario generar una
+excepción ArgumentException.
+
+double[,]? Suma(double[,] A, double[,] B)
+double[,]? Resta(double[,] A, double[,] B)
+double[,] Multiplicacion(double[,] A, double[,] B)
+*/
