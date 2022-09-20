5536d9e5f8dc9a0802f0f031ba6527a7837fa188
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:00:52 -0300

    Create ejer10.cs

---
+++ Practica/practica3/ejer10.cs
@@ -0,0 +1,5 @@
+/*
+10. Verificar con un par de ejemplos si la sección opcional [:formatString] de formatos compuestos
+redondea o trunca cuando se formatean números reales restringiendo la cantidad de decimales.
+Plantear los ejemplos con cadenas de formato compuesto y con cadenas interpoladas.
+*/
