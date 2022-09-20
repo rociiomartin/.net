dc975cd0528c402191c423f249908b526244cd82
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:23:43 -0300

    Create ejer12.cs

---
+++ Practica/practica4/ejer12.cs
@@ -0,0 +1,17 @@
+/*
+12) Completar la clase Cuenta para que el siguiente código produzca la salida indicada:
+
+Cuenta cuenta = new Cuenta();
+cuenta.Imprimir();
+cuenta = new Cuenta(30222111);
+cuenta.Imprimir();
+cuenta = new Cuenta("José Perez");
+cuenta.Imprimir();
+cuenta = new Cuenta("Maria Diaz", 20287544);
+cuenta.Imprimir();
+cuenta.Depositar(200);
+cuenta.Imprimir();
+cuenta.Extraer(150);
+cuenta.Imprimir();
+cuenta.Extraer(1500);
+*/
