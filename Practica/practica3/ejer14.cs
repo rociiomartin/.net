bd50be4d1c92db036bf7e2418dcb546500a3d0e8
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:03:04 -0300

    Create ejer14.cs

---
+++ Practica/practica3/ejer14.cs
@@ -0,0 +1,8 @@
+/*
+14. Utilizar la clase Stack<T> (pila) para implementar un programa que pase un número en base
+10 a otra base realizando divisiones sucesivas. Por ejemplo para pasar 35 en base 10 a binario
+dividimos sucesivamente por dos hasta encontrar un cociente menor que el divisor, luego el
+resultado se obtiene leyendo de abajo hacia arriba el cociente de la última división seguida por
+todos los restos.
+(imagen ejemplo)
+*/
