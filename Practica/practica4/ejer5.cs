2ac178a7edaa61228e263b82f0d3fce33b872d62
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2022-09-20 17:11:46 -0300

    Create ejer5.cs

---
+++ Practica/practica4/ejer5.cs
@@ -0,0 +1,13 @@
+/*
+5) Agregar un segundo constructor a la clase Hora para que pueda especificarse la hora por medio de un único
+valor que admita decimales. Por ejemplo 3,5 indica la hora 3 y 30 minutos. Si se utiliza este segundo
+constructor, el método imprimir debe mostrar los segundos con tres dígitos decimales. Así el siguiente código
+debe producir la salida por consola que se observa.
+
+new Hora(23, 30, 15).Imprimir();
+new Hora(14.43).Imprimir();
+new Hora(14.45).Imprimir();
+new Hora(14.45114).Imprimir();
+
+(muestra como se ve en el monitor tmb)
+*/
